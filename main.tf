58fc6847dc9e9bb9cc2454af7818e4bac59483f5
sti291271 <42182742+sti291271@users.noreply.github.com>
2020-12-22 14:06:52 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+/*
+resource "aws_s3_bucket" "seb_bucket" {
+  bucket = "my-tf-certif-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "Certification"
+    Environment = "test"
+  }
+}
+*/
