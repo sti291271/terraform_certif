74143f7bdef04d768168be9a863b541a1284d555
sti291271 <42182742+sti291271@users.noreply.github.com>
2021-01-13 16:10:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Test code
-
+/*
 resource "aws_s3_bucket" "seb_bucket" {
   bucket = "my-tf-certif-bucket"
   acl    = "private"
@@ -14,3 +14,4 @@ resource "aws_s3_bucket_object" "seb_bucket_object" {
   bucket = aws_s3_bucket.seb_bucket.id
   key        = "seb_test"
 }
+*/
