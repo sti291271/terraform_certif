7128deb3c8c91915ca77086d5282fcb43f68eb3a
sti291271 <42182742+sti291271@users.noreply.github.com>
2021-01-19 14:36:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Test code
-/*
 resource "aws_s3_bucket" "seb_bucket" {
+  count = var.condition ? 1 : 0
   bucket = "my-tf-certif-bucket"
   acl    = "private"
 
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "seb_bucket" {
 }
 
 resource "aws_s3_bucket_object" "seb_bucket_object" {
+  count = var.condition ? 1 : 0
   bucket = aws_s3_bucket.seb_bucket.id
   key        = "seb_test"
 }
-*/
