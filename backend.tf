c3fc9889862c8f93af8792e5fdc245bdeab7432e
sti291271 <42182742+sti291271@users.noreply.github.com>
2024-09-10 16:49:53 +0200

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "my-bucket-tf-backend"
+    key    = "AWSDynamoDB/01725979000272-84542b18/data/ov4nobrqke35zfseh2gsc2b3ky.json.gz"
+    region = "eu-east-3"
+  }
+}
