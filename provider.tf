8ef6193147809cd39e4a82721689b5107c47d663
sti291271 <42182742+sti291271@users.noreply.github.com>
2024-10-01 13:16:41 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.location1
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region = "eu-west-3"
+  access_key = "AKIA5G5PLNDM2MQAUYS4"
+  secret_key = "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"
 }
